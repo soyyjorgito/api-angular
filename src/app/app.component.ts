e50be624e05de67a0f2bd69e8c22ea16ec89196d
Jorge Eslava <jorge08432@hotmail.com>
2023-03-12 17:48:21 -0500

    Agregar botones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'api-angular';
+
+  constructor(private router: Router)
+  {
+    
+  }
+  Listar()
+    {
+      this.router.navigate(["listar"])
+    }
+  Nuevo()
+    {
+      this.router.navigate(["add"])
+    }
 }
