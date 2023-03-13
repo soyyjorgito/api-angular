21467f9df25363c61d4b6349e5878472cac7abe9
Jorge Eslava <jorge08432@hotmail.com>
2023-03-13 16:22:18 -0500

    Boton Agregar

--- src/app/Persona/add/add.component.ts
+++ src/app/Persona/add/add.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Tarea, TareaService } from 'src/app/Service/service.service';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css']
 })
-export class AddComponent {
+export class AddComponent implements OnInit{
 
+  tareaNueva: Tarea={
+    id:'',
+    tarea:'',
+    finalizado:false
+  }
+  ngOnInit(): void {
+   
+  }
+  constructor(private tareaService: TareaService,private router:Router)
+  {
+    
+  }
+
+  agregarTarea()
+  {
+    this.tareaService.saveTarea(this.tareaNueva).subscribe(
+
+      res=>
+      {
+        console.log(res);
+        this.router.navigate(['/listar']);
+      }
+    );
+  }
 }
